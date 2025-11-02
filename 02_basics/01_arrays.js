cdbbccc1b5e77c70cc45abd1b89189f154cd28c7
abdur.rahman14052003 <abdur.rahman14052003@gmail.com>
2025-11-02 14:02:46 +0000

    init

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,45 @@
+// // array
+
+const myArr = [0, 1, 2, 3, 4, 5, true, "AR"]
+
+// const myArr2 = new Array(1, 2, 3)
+
+// console.log(myArr2);
+
+// // array methods
+// myArr2.push(4);
+// console.log(myArr2);
+// myArr2.pop();
+// console.log(myArr2);
+// myArr2.unshift(10);
+// console.log(myArr2);
+// console.log(myArr2.indexOf(10));
+// console.log(myArr2.includes(2));
+
+// const newArr3 = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr3);
+// console.log(typeof myArr);
+// console.log(typeof newArr3);
+
+// slice, splice
+console.log("A ", myArr);
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("B ", myArr);
+
+const myArr4 = myArr.splice(1,3)
+console.log("C ", myArr);
+console.log(myArr4);
+
+
+
+
+
+
+
+
+
+
+
